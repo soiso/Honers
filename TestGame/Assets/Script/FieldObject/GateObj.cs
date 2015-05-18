77233e7ac4db9b219018ddadd21274c3f128796e
murata <heartex_0720@yahoo.co.jp>
2015-05-18 10:38:14 +0900

    新しいステージ１

---
+++ TestGame/Assets/Script/FieldObject/GateObj.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateObj : FieldObjectInterface 
+{
+    [SerializeField, HeaderAttribute("花を咲かせる回数")]
+    public int m_TargetNum;
+    private int m_CurrentNum;
+    private bool m_once;
+    private bool m_isSporn;
+
+    private TimeZone_BoxCollider m_TimeTrigger;
+
+    [SerializeField, HeaderAttribute("フルーツ出現位置")]
+    private Transform m_SpornPoint;
+
+    [SerializeField, HeaderAttribute("出現フルーツ")]
+    private GameObject[] m_Fruit;
+
+    private FruitArrangeManager m_owner;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_TimeTrigger = GetComponentInChildren<TimeZone_BoxCollider>();
+        m_owner = GetComponentInParent<FruitArrangeManager>();
+        m_CurrentNum = 0;
+        m_once = false;
+        m_isSporn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (this.transform.root.GetComponent<PicturePaper>().m_move)
+            return;
+
+        m_CurrentTimeZone = m_TimeTrigger.m_myColliderTimeZone;
+        //GetComponent<ShaderChanger>().Change(m_CurrentTimeZone);
+
+        if( Is_ActiveTimeZone( m_CurrentTimeZone ) )
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+
+            if (!m_once)
+            {
+                m_CurrentNum++;
+            }
+            m_once = true;
+
+            if( m_CurrentNum == m_TargetNum && !m_isSporn)
+            {
+                //レアフルーツ生成したいなぁ
+                SpornFruit(0);
+                m_isSporn = true;
+            }
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            m_once = false;
+        }
+	}
+
+    void SpornFruit( int type )
+    {
+        GameObject insert = m_owner.m_factory.Create_Object(type);
+        insert.transform.position = m_SpornPoint.position;
+        insert.transform.rotation = m_SpornPoint.rotation;
+        if (insert.GetComponent<FruitInfomation>().fruit_type == Fruit.FRUIT_TYPE.apple)
+        {
+            Quaternion q = Quaternion.AngleAxis(180, new Vector3(0, 1, 0));
+            insert.transform.rotation = q;
+        }
+    }
+}
---
+++ TestGame/Assets/Script/FieldObject/Mole.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mole : FieldObjectInterface 
+{
+    private TimeZone_BoxCollider m_TimeTrigger;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_TimeTrigger = GetComponentInChildren<TimeZone_BoxCollider>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (this.transform.root.GetComponent<PicturePaper>().m_move)
+            return;
+
+        m_CurrentTimeZone = m_TimeTrigger.m_myColliderTimeZone;
+        //GetComponent<ShaderChanger>().Change(m_CurrentTimeZone);
+
+        if( Is_ActiveTimeZone( m_CurrentTimeZone ) )
+        {
+            GetComponent<BoxCollider>().enabled = true;
+        }
+        else
+        {
+            GetComponent<BoxCollider>().enabled = false;
+        }
+	}
+}
