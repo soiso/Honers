1a1766f2c2229043a8192668efb0641586766b96
murata <heartex_0720@yahoo.co.jp>
2015-05-22 12:31:32 +0900

    幽霊スーッと消えてスーッと現れる

--- TestGame/Assets/Script/FieldObject/Cementery.cs
+++ TestGame/Assets/Script/FieldObject/Cementery.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class Cementery : FieldObjectInterface {
 
 
-    public bool m_is_active { get; private set; }
+    public bool m_is_active { get;  set; }
+    private bool m_isonce;
     private TimeZone_BoxCollider m_timezoneCollider;
 	// Use this for initialization
 	void Start () 
     {
         m_is_active = false;
-
+        m_isonce = false;
         m_timezoneCollider = GetComponentInChildren<TimeZone_BoxCollider>();
 
 	}
@@ -20,13 +21,15 @@ public class Cementery : FieldObjectInterface {
     {
         Material m = GetComponentInChildren<MeshRenderer>().material;
         m_CurrentTimeZone = m_timezoneCollider.m_myColliderTimeZone;
-	    if(Is_ActiveTimeZone(m_CurrentTimeZone))
+	    if(Is_ActiveTimeZone(m_CurrentTimeZone) && !m_isonce)
         {
+            m_isonce = true;
             m_is_active = true;
             m.color = Color.red;
         }
-        else
+        if( Is_SleepTimeZone(m_CurrentTimeZone))
         {
+            m_isonce = false;
             m_is_active = false;
             m.color = Color.black;
         }
