dbab484f9f62f13b7d23855aa14df7191c1e02d8
shunsuke <kwgcr368@ybb.ne.jp>
2015-06-01 08:03:04 +0900

    karasuできた？

--- TestGame/Assets/Script/Raven/EventRaven.cs
+++ TestGame/Assets/Script/Raven/EventRaven.cs
@@ -7,8 +7,13 @@ public class EventRaven : MonoBehaviour {
     private Transform[] m_movetargetTransForm;
     private FruitDropper m_dropper;
 
+    [SerializeField, HeaderAttribute("移動スピード")]
+    private float move_speed = 1.0f;
+
     Animator m_animator;
 
+
+    private bool m_drop = false;
 
 	// Use this for initialization
 	void Start ()
@@ -21,7 +26,14 @@ public class EventRaven : MonoBehaviour {
     void Move()
     {
         Vector3 vec = m_movetargetTransForm[1].position - m_movetargetTransForm[0].position;
-        this.transform.position += vec.normalized * Time.deltaTime;
+        this.transform.position += vec.normalized * Time.deltaTime * move_speed;
+
+        Vector3 d = this.transform.position - m_movetargetTransForm[1].position;
+        if(d.magnitude < 0.1f && !m_drop)
+        {
+            m_dropper.Drop();
+            m_drop = true;
+        }
 
     }
 	
