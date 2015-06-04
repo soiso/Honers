65c141577eb454c83f4bc17ba7ec465b8e47598b
murata <heartex_0720@yahoo.co.jp>
2015-06-04 09:53:35 +0900

    カラスモーションと出現位置 プレイヤー落ちても大丈夫 ステージの穴とひまわり

---
+++ TestGame/Assets/Script/Raven/EventRavenAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventRavenAnimation : StateMachineBehaviour
+{
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("isDrop", false);
+    }
+}
