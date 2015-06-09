9c28eb754ef6ddd557b3fb7ec6a03551bbb7f396
shunsuke <kwgcr368@ybb.ne.jp>
2015-06-09 12:25:42 +0900

    ステージ3修正

--- TestGame/Assets/Script/Trigger/himawaritrigger.cs
+++ TestGame/Assets/Script/Trigger/himawaritrigger.cs
@@ -5,15 +5,32 @@ public class himawaritrigger : MonoBehaviour {
 
     bool m_isActive = false;
 
+    GameObject m_player = null;
+    Vector3 m_player_LossyScale;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+	    if(m_player)
+        {
+            ScaleAdjust();
+        }
 	}
+
+    void ScaleAdjust()
+    {
+        Vector3 scale;
+        scale.x = m_player.transform.localScale.x / m_player.transform.lossyScale.x * m_player_LossyScale.x;
+        scale.y = m_player.transform.localScale.y / m_player.transform.lossyScale.y * m_player_LossyScale.y;
+        scale.z = m_player.transform.localScale.z / m_player.transform.lossyScale.z * m_player_LossyScale.z;
+
+        m_player.transform.localScale = scale;
+  
+    }
 
     public void OnTriggerEnter(Collider other)
     {
@@ -21,6 +38,8 @@ public class himawaritrigger : MonoBehaviour {
         if (layer_name == "Player")
         {
             other.gameObject.transform.parent = this.transform;
+            m_player = other.gameObject;
+            m_player_LossyScale = m_player.transform.lossyScale;
         }
     }
 
@@ -34,11 +53,9 @@ public class himawaritrigger : MonoBehaviour {
             Vector3 pos = other.gameObject.transform.position;
             pos.z = 0f;
             other.gameObject.transform.position = pos;
+            m_player.transform.localScale = Vector3.one;
+            m_player = null;
         }
     }
-
-
-
-
 
 }
