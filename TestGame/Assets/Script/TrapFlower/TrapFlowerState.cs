9233ec4c2b3c367b47a0ae75455906299052a9c8
shunsuketerauchi <kwgcr368@gmail.com>
2015-06-24 12:44:37 +0900

    ランキング差し替え

--- TestGame/Assets/Script/TrapFlower/TrapFlowerState.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TrapFlowerState : StateMachineBehaviour
-{
-
-    public virtual void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) { }
-    public virtual void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) { }
-}
